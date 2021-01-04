9e8e924be024a39fe2b8025e14f12b52214466a7
Houjun Liu <hliu@shabang.cf>
2021-01-03 21:55:03 -0800

    I mean it kinda works

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
-import Head from 'next/head'
+import ReactMarkdown from 'react-markdown'
+import * as matter from 'gray-matter';
+
+const gfm = require('remark-gfm')
+const anotherFile = matter(require('../KBBackup3/2020BIO101/KBhBIO101HumanDiseases.md').default)
 
 export default function Home() {
   return (
-      <span className="block shadow-lg font-bold p-3 text-red-500">Taproot</span>
+      <>
+          <span className="block shadow-lg font-bold p-3 text-red-500 text-lg">Taproot</span>
+          <ReactMarkdown plugins={[gfm]} children={anotherFile.content}>
+          </ReactMarkdown>
+      </>
   )
 }
