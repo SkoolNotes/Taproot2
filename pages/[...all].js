5b9528f0ec35913186297b1874fe212de2cdabb0
Houjun Liu <hliu@shabang.cf>
2021-01-05 22:56:49 -0800

    cool

--- pages/[...all].js
+++ pages/[...all].js
@@ -1,22 +1,24 @@
 import ReactMarkdown from 'react-markdown'
 import 'katex/dist/katex.min.css';
-import TeX from '@matejmazur/react-katex';
 import { useRouter } from 'next/router'
 import matter from 'gray-matter';
 import math from 'remark-math'
 
+import TeX from '@matejmazur/react-katex';
+
+
 const gfm = require('remark-gfm')
 
 const renderers = {
-    inlineMath: ({value}) => <Tex math={value} />,
-    math: ({value}) => <Tex block math={value} />,
+    inlineMath: ({value}) => <TeX math={value} />,
+    math: ({value}) => <TeX block math={value} />,
     text: ({value}) => {
+        // URL Renderer
         let v = value.replace(/\[\[\w*?]]/gi, function(x) {
             let docPointer = x.slice(2, -2);
-            //console.log(docPointer);
             if (docPointer) {
                 let url = `./${docPointer}`;
-                return `<a href=${url} class="text-red-600">[[${docPointer}]]</a>`;
+                return `<a href=${url} class="text-red-600 hover:text-red-800 transition">[[${docPointer}]]</a>`;
             } else {
                 console.log(`Cannot parse , returning...`);
                 return x;
@@ -37,7 +39,12 @@ const PageRenderer = () => {
         let theFile = matter(file)
         //console.log((matter(require(`../../KBBackup3/${course}/${note}.md`).default)).content)
 
-        return <ReactMarkdown plugins={[gfm, math]} children={theFile.content} renderers={renderers}/>
+        return (<>
+            <span className="sticky block shadow-lg font-bold p-4 text-red-500 text-xl top-0 bg-gray-100 z-40">Taproot</span>
+            <div className="content">
+                <ReactMarkdown plugins={[gfm, math]} children={theFile.content} renderers={renderers}/>
+            </div>
+        </>)
     } else {
         return null;
     }
