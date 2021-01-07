65a67f7b4e9a9124dc2b409502f952a1d9de50e2
Houjun Liu <hliu@shabang.cf>
2021-01-06 22:57:27 -0800

    wel

--- pages/[...all].js
+++ pages/[...all].js
@@ -42,10 +42,10 @@ const PageRenderer = () => {
         //console.log((matter(require(`../../KBBackup3/${course}/${note}.md`).default)).content)
 
         return (<>
-            <span className="sticky block shadow-lg font-bold p-4 text-red-500 text-xl top-0 bg-gray-100 z-40">Taproot <span style={{float: "right", fontWeight: 500}}>{theFile.data.title}</span></span>
+            <span className="sticky block shadow-lg font-bold p-4 text-red-500 text-xl top-0 bg-gray-100 dark:bg-gray-dark z-40">Taproot <span style={{float: "right", fontWeight: 500}}>{theFile.data.title}</span></span>
             <div className="content">
                 <div className="datarow">
-                    <span className="datarow-author">{theFile.data.author}</span> | <span className="datarow-course">{theFile.data.course}<span className="datarow-back"><a href={`./${theFile.data.source}`} class="text-red-500 hover:text-red-800 transition" style={{display: theFile.data.source?"inline":"none"}}>[[{theFile.data.source}]]</a><FontAwesomeIcon icon={faCaretRight} style={{transform: "translateY(2px)", margin: "0 6px", display: theFile.data.source?"inline":"none"}}/><a href={`./${note}`} class="text-red-500 hover:text-red-800 transition">[[{note}]]</a></span></span>
+                    <span className="datarow-author">{theFile.data.author}</span> | <span className="datarow-course">{theFile.data.course}<span className="datarow-back"><a href={`./${theFile.data.source}`} className="text-red-500 hover:text-red-800 transition" style={{display: theFile.data.source?"inline":"none"}}>[[{theFile.data.source}]]</a><FontAwesomeIcon icon={faCaretRight} style={{transform: "translateY(2px)", margin: "0 6px", display: theFile.data.source?"inline":"none"}}/><a href={`./${note}`} className="text-red-500 hover:text-red-800 transition">[[{note}]]</a></span></span>
                 </div>
                 <ReactMarkdown plugins={[gfm, math]} children={theFile.content} renderers={renderers}/>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 module.exports = {
-  purge: ['./pages/**/*.js', './components/**/*.js'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: ['./pages/**/*.js', './components/**/*.js'],
+    darkMode: 'media',
+    theme: {
+        extend: {
+            colors: {
+                gray: {
+                    darkest: '#171717',
+                    dark: '#1c1c1c',
+                    lightdark: '#262626',
+                    DEFAULT: '#c0ccda',
+                    light: '#e0e6ed',
+                    lightest: '#f9fafc',
+                }
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
