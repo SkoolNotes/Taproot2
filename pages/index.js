ae04651abca249403050d70cf251ab7407775a9c
Houjun Liu <hliu@shabang.cf>
2021-04-27 23:42:43 -0700

    more

--- pages/index.js
+++ pages/index.js
@@ -10,17 +10,31 @@ export default function Home() {
               <div className="mt-5 content">
 
                   <h1 className="mr-3">Taproot</h1>
+                  <div className="inline-block text-xs text-gray-500">(c) 2021 Houjun Liu, Huxley Marvit, Zachary Sayyah, Albert Huang, Dylan Wallace. </div>
+                  <br />
 
-                  Howdy 👋, welcome to Taproot. Take a look around, either in person or from our handy web portal.
-                  Philosophy
+                  Howdy 👋, welcome to Taproot. Take a look around, either <a href="https://github.com/skoolnotes/taproot" className="text-red-600 underline hover:text-red-400 transition-all">in person</a> or from our handy web portal, which you are currently at.
+
+
+                  <br />
+                  <br />
+
+                  <h2>Philosophy</h2>
 
                   Zettelkasten, maybe. But basically, create a repository of knowledge that should be easy to refer back to and effective for relearning things. We strive to create atomic, self contained notes that link to other references. Think a more granular Wikipedia
-                  Structure
+                  <br />
+                  <br />
 
-                  At the moment, Taproot is organized by the course that each concept falls into. The project was started with Zettelkasten style IDs prefixed with "KB", but was soon moved to semantic naming. Some files are named according to the convention outlined here:
 
-                  [[KBeMetaIndex]] @Exr0n
-                  [[KBhMETAindex]] @Jemoka
+                  <h2>Structure</h2>
+
+                  At the moment, Taproot is organized by the course that each concept falls into. The project was started with Zettelkasten style IDs prefixed with "KB", but was soon moved to semantic naming. 
+
+                  <br />
+                  <br />
+
+
+                  <hr />
               </div>
           </div>
 
